03f2c3cf7022d1e2fec67e5b4300d7b8e6896891
payelchandra <chandrapayel03@gmail.com>
2025-09-20 16:40:02 +0530

    dd

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import HeroSection2 from '../components/HeroSection2'
-import Leader from '../components/Leader'
-import ProductsSection from '../components/ProductSection'
-import Contact from './Contact.jsx'
+import React from "react";
+import Hero from "../components/Hero";
+import HeroSection2 from "../components/HeroSection2";
+import Leader from "../components/Leader";
+import ProductsSection from "../components/ProductSection"; 
 function LandingPage() {
-    return (
-        /* <Hero />
-        <HeroSection2 />
-        <ProductsSection/>
-        <Leader/> */
-        <>
-            <Contact/>
-        </>
-    )
+  return (
+    <>
+      <Hero />
+      <HeroSection2 />
+      <ProductsSection />
+      <Leader />
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
